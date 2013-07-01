628a43b7c55e414530cd7f530141e3c1a5da501d
Random Ambersky <RandomAmbersky@gmail.com>
2013-07-01 06:49:12 +0400

    показывается лог в текстовой консоли

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 
 #include <QFileDialog>
+#include <QProcess>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -22,8 +24,34 @@ void MainWindow::on_actionExit_triggered()
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
-                                                     ".",
-                                                     QFileDialog::ShowDirsOnly
-                                                     | QFileDialog::DontResolveSymlinks);
+//    _dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+//                                                     ".",
+//                                                     QFileDialog::ShowDirsOnly
+//                                                     | QFileDialog::DontResolveSymlinks);
+
+    _dir = "../GamePlay";
+
+    showLog();
+
+}
+
+void MainWindow::showLog()
+{
+
+    QProcess log;
+    log.setWorkingDirectory( _dir );
+    qDebug() << "working dir:" << log.workingDirectory();
+    //TODO ограничение в 10 коммитов!
+    log.start("git", QStringList() << "log" << "--oneline" << "--abbrev-commit" << "--all" << "--decorate=full" << "-n 10" << "--children" );
+    if (!log.waitForStarted()) return;
+
+    //log.write("Qt rocks!");
+    log.closeWriteChannel();
+
+    if (!log.waitForFinished()) return;
+
+    QByteArray result = log.readAll();
+
+    ui->textEdit->setText( result );
+
 }
